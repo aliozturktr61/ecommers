dff892082b008ff43648b49eaaf2bcef6a95aaee
Ali Öztürk <aliozturktr61@gmail.com>
2024-06-03 22:46:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+import { addToCart, displayCartTotal, renderCartItems } from "./js/cart.js";
+import { fetchProducts, renderProducts } from "./js/products.js";
+
+//*olay izleyicisi ekledik
+document.addEventListener("DOMContentLoaded", async() => {
+ 
+  if (window.location.pathname.includes("card.html")) 
+    {
+    renderCartItems();
+    displayCartTotal();
+  } 
+  else
+   {
+    //eğer sayfa cart.htm sayfasında değilse ürünleri al
+    const products= await fetchProducts();
+
+//ürünleri render et ve addtocartcallback fonk tanımla 
+    renderProducts(products,(event)=>addToCart(event,products));
+  }
+});
+
